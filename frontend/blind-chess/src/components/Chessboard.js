5a4b0a3d41fc223ca7815ac28015e464a28e2053
kananb <kananboubion@gmail.com>
2022-01-30 21:56:21 -0800

    Fix board coordinates

--- frontend/blind-chess/src/components/Chessboard.js
+++ frontend/blind-chess/src/components/Chessboard.js
@@ -3,22 +3,22 @@ import React from 'react';
 function Chessboard(props) {
 	const board = [];
 
-	const ranks = ["h", "g", "f", "e", "d", "c", "b", "a"];
+	const ranks = ["a", "b", "c", "d", "e", "f", "g", "h"];
 	for (let r = 0; r < 8; r++) {
 		const squares = [];
 		for (let c = 0; c < 8; c++) {
 			const off = r&1 ? 1 : 0;
 			const color = (r*8+c+off)&1 ? "dark" : "light";
 
-			const file = r === 7 ? c : "";
-			const rank = c === 0 ? ranks[r] : "";
+			const file = r === 7 ? ranks[c] : "";
+			const rank = c === 0 ? (8-r) : "";
 			squares.push(
 				<td key={c} className={ "square "+color }>
-					<div className="file">
-						{ file }
-					</div>
 					<div className="rank">
 						{ rank }
+					</div>
+					<div className="file">
+						{ file }
 					</div>
 				</td>
 			);
