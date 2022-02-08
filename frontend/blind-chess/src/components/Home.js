efaf29b2c72187601b8addf10e5a2586b28bc330
kananb <kananboubion@gmail.com>
2022-02-08 14:06:09 -0800

    Implement proper socket connection handling code

--- frontend/blind-chess/src/components/Home.js
+++ frontend/blind-chess/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Menu from './Menu';
 import Game from './Game';
 import Chessboard from './Chessboard';
@@ -6,26 +6,32 @@ import Chessboard from './Chessboard';
 function Home(props) {
 	const [conn, setConn] = useState(null);
 	const [code, setCode] = useState(undefined);
+	const timeout = useRef(250);
 
 	useEffect(() => {
-		let socket = new WebSocket(`ws${(window.location.protocol === "https:") ? "s" : ""}://${window.location.host}/game`);
-
-		socket.onopen = () => {
-			console.log("Opened websocket connection");
-			setConn(socket);
-		};
-
-		socket.onclose = () => {
-
-		};
-
-		socket.onerror = () => {
-
-		};
-
-		return () => {
-			socket.close();
-		};
+		(function connect() {
+			let socket = new WebSocket(`ws${(window.location.protocol === "https:") ? "s" : ""}://${window.location.host}/game`);
+			let connectInterval;
+	
+			socket.onopen = () => {
+				console.log("Socket connection established.");
+				timeout.current = 250;
+				clearTimeout(connectInterval);
+				setConn(socket);
+			};
+	
+			socket.onclose = () => {
+				timeout.current += timeout.current;
+				connectInterval = setTimeout(() => {
+					if (!conn || conn.readyState === WebSocket.CLOSED) connect();
+				}, Math.min(timeout.current, 10000));
+			};
+	
+			socket.onerror = () => {
+				console.error("Socket encountered an error, retrying connection.");
+				socket.close();
+			};
+		})();
 	}, []);
 
 	const handleJoin = id => {
